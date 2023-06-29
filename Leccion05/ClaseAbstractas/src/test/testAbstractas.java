f119941e650277705cf0f3c507230472fd0d7ca4
Franco Ariel <103206342+Chorifrann@users.noreply.github.com>
2023-06-29 20:02:45 -0300

    Add files via upload

---
+++ Leccion05/ClaseAbstractas/src/test/testAbstractas.java
@@ -0,0 +1,12 @@
+
+package test;
+
+import domain.*;
+
+public class testAbstractas {
+    public static void main(String[] args) {
+        FiguraGeometrica figura = new rectangulo("Rectangulo");
+        figura.dibujar();
+    }
+    
+}
